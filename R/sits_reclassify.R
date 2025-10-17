10e16e48bb7fe517da93adb99ec18de5da5debde
Felipe Carlos <efelipecarlos@gmail.com>
2025-10-17 13:54:49 -0300

    reclassify: update proc_bloat

--- R/sits_reclassify.R
+++ R/sits_reclassify.R
@@ -158,7 +158,7 @@ sits_reclassify.class_cube <- function(cube, ...,
         block_size = .block_size(block = block, overlap = 0L),
         npaths = 2L,
         nbytes = 4L,
-        proc_bloat = 2
+        proc_bloat = 4
     )
     # Update multicores parameter
     multicores <- .jobs_max_multicores(
